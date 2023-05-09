3e350289b90ac14acac9459d1b7a976ea13473ac
unknown <faiz.jamaludin02@gmail.com>
2023-05-09 23:00:57 +0800

    add review section for user to comment

--- src/app/pages/restaurant/restaurant.component.ts
+++ src/app/pages/restaurant/restaurant.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import {
+  faLocationDot,
+  faMobileRetro,
+  faEnvelope,
+  faStar,
+  faStarHalfStroke,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-restaurant',
   templateUrl: './restaurant.component.html',
-  styleUrls: ['./restaurant.component.css']
+  styleUrls: ['./restaurant.component.css'],
 })
 export class RestaurantComponent {
+  // declare icons
 
+  faLocationDot = faLocationDot;
+  faMobileRetro = faMobileRetro;
+  faEnvelope = faEnvelope;
+  faStar = faStar;
+  faStarHalfStroke = faStarHalfStroke;
 }
