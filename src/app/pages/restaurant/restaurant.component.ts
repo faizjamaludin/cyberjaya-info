ada6e8e80d0761fa42546f6f4c9084a745af5abf
Faiz Jamaludin <faiz.jamaludin02@gmail.com>
2023-06-07 12:09:55 +0800

    fetch the data for opening hours and can be display in looping

--- src/app/pages/restaurant/restaurant.component.ts
+++ src/app/pages/restaurant/restaurant.component.ts
@@ -30,21 +30,32 @@ export class RestaurantComponent implements OnInit {
 
   // iniatializer
   id: string | null = '';
-  listings: any[] = [];
+  listings: any;
+  openingHours: any;
+  from: string | null = '';
+  to: string | null = "";
 
   constructor(
     private http: HttpClient,
     private route: ActivatedRoute,
     private datePipe: DatePipe
-  ) {}
+  ) { }
+
+
 
   ngOnInit(): void {
+
     this.id = this.route.snapshot.paramMap.get('id');
     const url = 'http://localhost:3001/listing/list/' + this.id;
 
     this.http.get(url).subscribe((response: any) => {
-      this.listings = [response.listing];
-      console.log(this.formatDate(this.listings[0].list_date));
+      // this.listings = [response.listing];
+      this.listings = response.listing;
+
+      this.openingHours = Object.keys(this.listings.list_openingHour)
+      console.log(this.openingHours)
+
+      console.log(this.capitalizeWords((this.listings).toString()))
     });
   }
 
@@ -52,4 +63,11 @@ export class RestaurantComponent implements OnInit {
   formatDate(date: string): string | null {
     return this.datePipe.transform(date, 'dd MMM yyyy');
   }
+
+  capitalizeWords = (str: string) => {
+    return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+  }
 }
+
+
+
